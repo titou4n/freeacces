442aac8d258c010c4833331dec7b168ef9396bd6
titou4n <titou4n.simon@gmail.com>
2022-06-05 20:02:30 +0200

    First commit

--- main.py
+++ main.py
@@ -11,29 +11,28 @@ def acceuil():
 
 @app.route('/signup')
 def signup():
-    cluster.insert_one({"mail":"mail"},{"password":"password"})
-    #if request.method == "mail" and :
-        #recuperer = request.form['mail']
-    return render_template('page2.html')
+    if request.method == 'POST':
+        recuperer_mail = request.form['mail']
+        recuperer_mdp = request.form['mdp']
+        cluster.insert_one({"mail":"mail"},{"password":"password"})
+        return render_template('pageconnected.html',mail=recuperer_mail,mdp=recuperer_mdp)
 
 @app.route('/signin')
 def signin():
-    #mail=str(mail)
-    #if mail  == :
-        return ("Je suis la page de connection")
-    
-    #pass #mail de la base
-    
+    if request.method == 'POST':
+        recuperer_mail = request.form['mail']
+        recuperer_mdp = request.form['mdp']
+        if recuperer_mail==cluster.find_one({"mail":"mail"}) and recuperer_mdp == cluster.find_one({"password":"password"}):
+            return render_template('pageconnected.html',mail=recuperer_mail,mdp=recuperer_mdp)
 
+@app.route('/connected')
+def salon():
+    return print("Je suis la page ou vous etes connectés")
 
 @app.route('/salon')
 def salon():
     return print("Je suis la page de disccussion")
-
-print("gouzi gouzi")
-print("gouzi gouzi")
-print("gouzi gouzi")
-print("gouzi gouzi")
+    
 #git commit -m "First commit"
 
 app.run(host='0.0.0.0')
