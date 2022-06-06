476b46f96abda799254a0e269ffbd84f8135d513
titou4n <titou4n.simon@gmail.com>
2022-06-06 16:14:09 +0200

    First commit

--- Data/database_handler.py
+++
@@ -1,6 +0,0 @@
-from flask_pymongo import PyMongo
-
-class DatabaseHandler():
-  def create_person(self, mail:str, password:str):
-    cluster.insert_one({"mail":"mail"},{"password":"password"})
-   
--- main.py
+++ main.py
@@ -1,37 +1,80 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
+import bcrypt
 
 app = Flask('app')
-cluster=PyMongo( app, "mongodb://freeacces:freeacces@cluster.mongode.net/admin")
+cluster=PyMongo( app, "mongodb://freeacces:freeacces@cluster.mongodb.net/admin")
 
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def acceuil():
-    return render_template('accueil.html')
+    if 'util' in session:
+        return render_template('accueil.html',nom=session['util'])
+    else:
+        return render_template('accueil.html')    
 
-@app.route('/signup')
-def signup():
-    if request.method == 'POST':
-        recuperer_mail = request.form['mail']
-        recuperer_mdp = request.form['mdp']
-        cluster.insert_one({"mail":"mail"},{"password":"password"})
-        return render_template('pageconnected.html',mail=recuperer_mail,mdp=recuperer_mdp)
+@app.route('/logout')
+def logout():
+	session.clear()
+	return redirect(url_for("index"))
 
-@app.route('/signin')
-def signin():
-    if request.method == 'POST':
-        recuperer_mail = request.form['mail']
-        recuperer_mdp = request.form['mdp']
-        if recuperer_mail==cluster.find_one({"mail":"mail"}) and recuperer_mdp == cluster.find_one({"password":"password"}):
-            return render_template('pageconnected.html',mail=recuperer_mail,mdp=recuperer_mdp)
 
-@app.route('/connected')
-def salon():
-    return print("Je suis la page ou vous etes connectés")
+@app.route('/login', methods=['POST','GET'])
+def login():
+	if request.method == 'POST':
+		base_donne = cluster.db.test
+		util = base_donne.find_one({'mail' : request.form['mail']})
+		if util :
+			if bcrypt.checkpw(request.form['mot_de_passe'].encode('utf-8'), util['mdp']):
+				session['util']= request.form['mail']
+				return redirect(url_for("acceuil"))
+			else:
+				return render_template('login.html',erreur="Le mot de passe est incorect")
+		else :
+			return render_template('login.html', erreur="Le nom d'utlisateur n'existe pas")
+	
+	else:
+		return render_template('login.html')
 
-@app.route('/salon')
-def salon():
-    return print("Je suis la page de disccussion")
+
+
+@app.route('/register', methods=['POST','GET'])
+def register():
+	if request.method == 'POST':
+		base_donne = cluster.db.test
+		if base_donne.find_one({'mail' : request.form['mail']}) :
+			return render_template('register.html', erreur="Le mail existe deja")
+		else :
+			mdp_encrypte = bcrypt.hashpw(request.form['mdp'].encode('utf-8'),bcrypt.gensalt())
+			base_donne.insert_one({'mail': request.form['mail'], 'mdp' : mdp_encrypte})
+			session['util'] = request.form['mail']
+			return redirect(url_for('acceuil'))
+	else:
+		return render_template('register.html')
+
+#@app.route('/signup', methods=['POST','GET'])
+#def signup():
+    #if request.method == 'POST':
+        #recuperer_mail = request.form['mail']
+        #recuperer_mdp = request.form['mdp']
+        #cluster.insert_one({"mail":"mail"},{"password":"password"})
+        #return render_template('pageconnected.html',mail=recuperer_mail,mdp=recuperer_mdp)
+
+#@app.route('/signin', methods=['POST','GET'])
+#def signin():
+    #if request.method == 'POST':
+       # recuperer_mail = request.form['mail']
+      #  recuperer_mdp = request.form['mdp']
+        #if recuperer_mail==cluster.find_one({"mail":"mail"}) and recuperer_mdp == cluster.find_one({"password":"password"}):
+           # return render_template('pageconnected.html',mail=recuperer_mail,mdp=recuperer_mdp)
+
+#@app.route('/connected', methods=['POST','GET'])
+#def connected():
+    #return print("Je suis la page ou vous etes connectés")
+
+#@app.route('/salon', methods=['POST','GET'])
+#def salon():
+    #return print("Je suis la page de disccussion")
     
 #git commit -m "First commit"
 
